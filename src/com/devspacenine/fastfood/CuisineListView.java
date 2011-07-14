9eb963ec7224ab51ea799ee6729bc4ef7b21d245
yeluapyeroc <devspace9@gmail.com>
2011-07-14 14:45:37 -0500

    Moving from laptop to desktop

--- src/com/devspacenine/fastfood/CuisineListView.java
+++ src/com/devspacenine/fastfood/CuisineListView.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CuisineListView extends ListActivity {
@@ -18,6 +19,7 @@ public class CuisineListView extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cuisine_list);
 		
 		// Instantiate the selectedCuisines ArrayList
 		selectedCuisines = new ArrayList<String>();
@@ -28,20 +30,21 @@ public class CuisineListView extends ListActivity {
 		
 		// Get the ListView, make it filterable, then set the OnItemClickListener
 		ListView lv = getListView();
+		lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 		lv.setTextFilterEnabled(true);
 		
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			// Called every time a list item is clicked
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				// Get the RelativeLayout and toggle its selection
-				RelativeLayout selected_layout = (RelativeLayout) view;
-				if(selected_layout.isSelected()) {
-					selectedCuisines.remove(position);
-					selected_layout.setSelected(false);
+				CheckedTextView selection = (CheckedTextView) view;
+				Log.d("DSN Debug", selection.toString());
+				if(selection.isChecked()) {
+					selectedCuisines.remove((String) selection.getText());
+					selection.setChecked(false);
 				}else{
-					TextView selected_text = (TextView) selected_layout.getChildAt(0);
-					selectedCuisines.add(position, (String) selected_text.getText());
-					selected_layout.setSelected(true);
+					selectedCuisines.add((String) selection.getText());
+					selection.setChecked(true);
 				}
 			}
 		});
